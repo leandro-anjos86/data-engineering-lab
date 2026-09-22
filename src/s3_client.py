b0d09a62b6fcf9f973931c1d0e07d72fe5d583b8
Leandro Anjos <leandro.anjos86@gmail.com>
2026-09-21 23:15:20 -0300

    ajustes de integração do python

--- src/s3_client.py
+++ src/s3_client.py
@@ -15,9 +15,9 @@ class S3ClientFactory:
     def get_client():
         return boto3.client(
             "s3",
-            endpoint_url=os.getenv("AWS_ENDPOINT_URL", "http://localhost:4566"),
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", "test"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", "test"),
+            endpoint_url=os.getenv("AWS_ENDPOINT_URL", "na"),
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", "na"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", ""),
             region_name=os.getenv("AWS_DEFAULT_REGION", "us-east-1"),
             config=Config(
                 signature_version="s3v4",
